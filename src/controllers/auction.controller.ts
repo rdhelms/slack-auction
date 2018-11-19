3163297a4ce552aa97d5ce80d0c986569baf2d38
Robert Helms <robert.helms@smashingboxes.com>
2018-11-19 13:57:28 -0500

    Added action payload handling

--- src/controllers/auction.controller.ts
+++ src/controllers/auction.controller.ts
@@ -1,7 +1,38 @@
 import express from 'express';
 const auctionController = express.Router();
 
-interface ISlackRequest { 
+interface ISlackAction {
+    payload: {
+        type: string;
+        actions: {
+            name: string;
+            value: string;
+            type: string;
+        }[];
+        callback_id: string;
+        team: {
+            id: string;
+            domain: string;
+        };
+        channel: {
+            id: string;
+            name: string;
+        };
+        user: {
+            id: string;
+            name: string;
+        },
+        action_ts: string;
+        message_ts: string;
+        attachment_id: string;
+        token: string;
+        original_message: ISlackRequest;
+        response_url: string;
+        trigger_id: string;
+    };
+}
+
+interface ISlackRequest {
     token: string;
     team_id: string;
     team_domain: string;
@@ -22,7 +53,15 @@ auctionController.route('/')
                 text: 'Invalid command'
             });
         }
-        const body = req.body as ISlackRequest;
+        const body = req.body as ISlackRequest | ISlackAction;
+
+        // If the user triggered an action (like a button) we will have a payload
+        if ('payload' in body) {
+            // TODO: Perform an actual update on the auction rather than reflecting the existing info
+            return res.json(body.payload.original_message);
+        }
+
+        // Check that the user provided all the necessary info
         if (!body.text || body.text.split(' ').length < 3) {
             return res.json({
                 text: 'You must provide an item name, price, and description!'
